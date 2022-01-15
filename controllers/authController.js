1db378a504f5375d5813ae857e5fa37726fa5539
Abhishek <abhishek.rathore@xaviers.edu.in>
2022-01-16 00:42:42 +0530

    /.

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,12 +16,12 @@ const createSendToken = (user, statusCode, req, res) => {
 
   const token = signToken(user._id);
   
-  res.cookie('jwt', token, {
+  res.cookie('jwt', 'token1223', {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    //secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
   });
   
   user.password = undefined;
