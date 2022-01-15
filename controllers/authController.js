c89c4911b8a4e9406194a38c88498cb53593f6d5
Abhishek <abhishek.rathore@xaviers.edu.in>
2022-01-16 00:23:57 +0530

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,23 +45,24 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
   
 exports.login = catchAsync(async (req, res, next) => {
+  
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+  
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+  
   res.cookie('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 10000),
     httpOnly: true
   });
   res.status(200).json({ status: 'success' });
   const { email, password } = req.body;
-  
-  // if (!email || !password) {
-  //   return next(new AppError('Please provide email and password!', 400));
-  // }
-
-  // // 2) Check if user exists && password is correct
-  // const user = await User.findOne({ email }).select('+password');
-  
-  // if (!user || !(await user.correctPassword(password, user.password))) {
-  //   return next(new AppError('Incorrect email or password', 401));
-  // }
   
   // // 3) If everything ok, send token to client
   // createSendToken(user, 200, req, res);
