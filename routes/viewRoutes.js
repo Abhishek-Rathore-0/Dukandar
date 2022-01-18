b317f529eaeaf8839a92dd0209a3af3e3108ecba
Abhishek <abhishek.rathore@xaviers.edu.in>
2022-01-18 21:31:05 +0530

    .

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -9,10 +9,5 @@ const router = express.Router();
 router.get("/", authController.isLoggedIn,viewsController.home);
 router.get("/account",authController.protect, viewsController.account);
 
-router.get("/agent",authController1.isLoggedIn, viewsController.shopkeeper);
-router.get("/agent/signup",authController1.isLoggedIn, viewsController.signup);
-router.get("/agent/account", authController1.protect, viewsController.agentaccount);
-router.get("/agent/products", authController1.protect, viewsController.products)
-  
 module.exports = router;
   
